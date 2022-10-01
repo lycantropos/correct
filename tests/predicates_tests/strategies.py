864291109800a6ec05f70f3c4c3cb40c77dff572
Azat Ibrakov <azatibrakov@gmail.com>
2022-10-01 22:52:06 +0200

    Complete strategies

--- tests/predicates_tests/strategies.py
+++ tests/predicates_tests/strategies.py
@@ -1,5 +1,6 @@
 import sys
 import typing
+from functools import partial
 from itertools import repeat
 
 from hypothesis import strategies
@@ -20,24 +21,30 @@ special_generic_aliases = strategies.sampled_from(
 )
 
 
+def pass_to_generic_alias(
+        arguments: SearchStrategy[Annotation],
+        alias: SpecialGenericAlias
+) -> SearchStrategy[Annotation]:
+    parameters_count = special_generic_alias_to_parameters_count(alias)
+    return (strategies.builds(alias.__getitem__,
+                              strategies.tuples(*repeat(arguments,
+                                                        parameters_count)))
+            if parameters_count
+            else arguments)
+
+
 def nest_annotations(
         base: SearchStrategy[Annotation]
 ) -> SearchStrategy[Annotation]:
-    return special_generic_aliases.flatmap(
-            lambda alias: strategies.builds(
-                    alias.__getitem__,
-                    strategies.tuples(
-                            *repeat(
-                                    base,
-                                    special_generic_alias_to_parameters_count(
-                                            alias
-                                    )
-                            )
-                    )
-            )
-            if special_generic_alias_to_parameters_count(alias)
-            else base
-    )
+    return (special_generic_aliases.flatmap(partial(pass_to_generic_alias,
+                                                    base))
+            | strategies.builds(typing.Optional.__getitem__, base)
+            | strategies.builds(typing.Union.__getitem__,
+                                (strategies.lists(base,
+                                                  min_size=1)
+                                 .map(tuple)))
+            | strategies.builds(typing.Tuple.__getitem__,
+                                strategies.lists(base).map(tuple)))
 
 
 if sys.version_info >= (3, 9):
@@ -69,7 +76,3 @@ plain_annotations = strategies.recursive(
         ),
         nest_annotations
 )
-plain_annotations |= strategies.builds(typing.Union.__getitem__,
-                                       (strategies.lists(plain_annotations,
-                                                         min_size=1)
-                                        .map(tuple)))
