701ed14df8d8a270bbc8cee2429cd3ef013b2158
Azat Ibrakov <azatibrakov@gmail.com>
2022-10-04 21:59:05 +0200

    Complete 'correct.predicates' module

--- correct/_core/predicates.py
+++ correct/_core/predicates.py
@@ -40,22 +40,6 @@ else:
 
 
 def is_subtype(left: Annotation, right: Annotation) -> bool:
-    """
-    Checks if annotation is a subtype of another.
-
-    >>> is_subtype(bool, int)  # types are invariant by default
-    False
-    >>> from typing import TypeVar
-    >>> CovariantInt = TypeVar('CovariantInt', bound=int, covariant=True)
-    >>> is_subtype(bool, CovariantInt)
-    True
-    >>> is_subtype(int, bool)
-    False
-    >>> ContravariantBool = TypeVar('ContravariantBool', bound=bool,
-    ...                             contravariant=True)
-    >>> is_subtype(int, ContravariantBool)
-    True
-    """
     if isinstance(left, t.TypeVar):
         left, left_variance = unpack_type_var(left), type_var_to_variance(left)
     else:
--- correct/predicates.py
+++ correct/predicates.py
@@ -1,3 +1,22 @@
-from ._core.predicates import is_subtype
+from ._core.hints import Annotation as _Annotation
+from ._core.predicates import is_subtype as _is_subtype
 
-__all__ = [is_subtype.__name__]
+
+def is_subtype(left: _Annotation, right: _Annotation) -> bool:
+    """
+    Checks if annotation is a subtype of another.
+
+    >>> is_subtype(bool, int)  # types are invariant by default
+    False
+    >>> from typing import TypeVar
+    >>> CovariantInt = TypeVar('CovariantInt', bound=int, covariant=True)
+    >>> is_subtype(bool, CovariantInt)
+    True
+    >>> is_subtype(int, bool)
+    False
+    >>> ContravariantBool = TypeVar('ContravariantBool', bound=bool,
+    ...                             contravariant=True)
+    >>> is_subtype(int, ContravariantBool)
+    True
+    """
+    return _is_subtype(left, right)
