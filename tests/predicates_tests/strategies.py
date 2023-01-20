9e1966522ecf205405cc1a52eb7a34ac34c39588
Azat Ibrakov <azatibrakov@gmail.com>
2023-01-20 22:20:41 +0100

    Complete strategies

--- tests/predicates_tests/strategies.py
+++ tests/predicates_tests/strategies.py
@@ -1,20 +1,21 @@
 import sys
-import typing
+import typing as t
 from functools import partial
 from itertools import repeat
 
 from hypothesis import strategies
 from hypothesis.strategies import SearchStrategy
+from paradigm.base import signature_from_callable
 
+from correct._core.predicates import is_generic_alias
 from correct._core.utils import to_base
 from correct.hints import Annotation
-from tests.utils import SpecialGenericAlias
+from tests.utils import GenericAlias
 
 special_generic_aliases_values = [
     candidate
-    for candidate in vars(typing).values()
-    if (isinstance(candidate, SpecialGenericAlias)
-        and candidate is not typing.Callable)
+    for candidate in vars(t).values()
+    if is_generic_alias(candidate) and candidate is not t.Type
 ]
 special_generic_aliases = strategies.sampled_from(
         special_generic_aliases_values
@@ -23,9 +24,9 @@ special_generic_aliases = strategies.sampled_from(
 
 def pass_to_generic_alias(
         arguments: SearchStrategy[Annotation],
-        alias: SpecialGenericAlias
+        alias: GenericAlias
 ) -> SearchStrategy[Annotation]:
-    parameters_count = special_generic_alias_to_parameters_count(alias)
+    parameters_count = generic_alias_to_parameters_count(alias)
     return (strategies.builds(alias.__getitem__,
                               strategies.tuples(*repeat(arguments,
                                                         parameters_count)))
@@ -38,30 +39,33 @@ def nest_annotations(
 ) -> SearchStrategy[Annotation]:
     return (special_generic_aliases.flatmap(partial(pass_to_generic_alias,
                                                     base))
-            | strategies.builds(typing.Optional.__getitem__, base)
-            | strategies.builds(typing.Union.__getitem__,
+            | strategies.builds(t.Optional.__getitem__, base)
+            | strategies.builds(t.Union.__getitem__,
                                 (strategies.lists(base,
                                                   min_size=1,
                                                   max_size=5)
                                  .map(tuple)))
-            | strategies.builds(typing.Tuple.__getitem__,
+            | strategies.builds(t.Callable.__getitem__,
+                                strategies.tuples(strategies.just(Ellipsis),
+                                                  base))
+            | strategies.builds(t.Callable.__getitem__,
+                                strategies.tuples(strategies.lists(base,
+                                                                   max_size=5),
+                                                  base))
+            | strategies.builds(t.Tuple.__getitem__,
                                 strategies.tuples(base,
                                                   strategies.just(Ellipsis)))
-            | strategies.builds(typing.Tuple.__getitem__,
+            | strategies.builds(t.Tuple.__getitem__,
                                 (strategies.lists(base,
                                                   max_size=5)
                                  .map(tuple))))
 
 
 if sys.version_info >= (3, 9):
-    def special_generic_alias_to_parameters_count(
-            value: SpecialGenericAlias
-    ) -> int:
+    def generic_alias_to_parameters_count(value: GenericAlias) -> int:
         return value._nparams
 else:
-    def special_generic_alias_to_parameters_count(
-            value: SpecialGenericAlias
-    ) -> int:
+    def generic_alias_to_parameters_count(value: GenericAlias) -> int:
         return len(value.__parameters__)
 
 special_generic_aliases_origins_values = {
@@ -73,8 +77,20 @@ def is_not_special_generic_alias_origin(value: type) -> bool:
     return value not in special_generic_aliases_origins_values
 
 
+def has_parseable_signature(value: type) -> bool:
+    try:
+        signature_from_callable(value)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 plain_static_annotations = strategies.recursive(
-        strategies.from_type(type).filter(is_not_special_generic_alias_origin),
+        strategies.from_type(type).filter(is_not_special_generic_alias_origin)
+        | strategies.builds(t.Type.__getitem__,
+                            (strategies.from_type(type)
+                             .filter(has_parseable_signature))),
         nest_annotations
 )
 type_variables_names = strategies.text()
@@ -83,12 +99,12 @@ type_variables_names = strategies.text()
 def to_variable_annotations(
         static_annotations: SearchStrategy[Annotation]
 ) -> SearchStrategy[Annotation]:
-    return (strategies.builds(typing.TypeVar, type_variables_names,
+    return (strategies.builds(t.TypeVar, type_variables_names,
                               bound=static_annotations)
-            | strategies.builds(typing.TypeVar, type_variables_names,
+            | strategies.builds(t.TypeVar, type_variables_names,
                                 bound=static_annotations,
                                 contravariant=strategies.booleans())
-            | strategies.builds(typing.TypeVar, type_variables_names,
+            | strategies.builds(t.TypeVar, type_variables_names,
                                 bound=static_annotations,
                                 covariant=strategies.booleans()))
 
